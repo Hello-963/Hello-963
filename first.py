9cf31e3896523a0431a7f63ad5d95c186ef73cf7
Aditya Choudhary <aditya.choudhary@betaque.com>
2025-01-17 15:51:37 +0530

    added and updated files

--- first.py
+++ first.py
@@ -1,4 +1,7 @@
 print("Hello")
-print("This is being written in another branch named demobrancho")
 print("Sample text for amend command that I want to add")
+print("Namaste")
+print("Hi")
+print("This is being written in another branch named demobrancho")
 branch = demobranch
+
