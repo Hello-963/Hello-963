1a2110f94f5f866db9581ad6d29c64973dbb6d6c
Aditya Choudhary <126869135+Hello-963@users.noreply.github.com>
2025-01-17 14:36:48 +0530

    Update first.py

--- first.py
+++ first.py
@@ -2,6 +2,7 @@ print("Hello")
 print("Sample text for amend command that I want to add")
 print("Namaste")
 print("Hi")
+name = Ram
 print("This is being written in another branch named demobrancho")
 branch = demobranch
 
